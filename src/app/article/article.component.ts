f1a6ae389e31849f20601266d0441e38696b3a0f
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-28 18:20:35 +0600

    Article model & functions added

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
+import {Article} from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,24 +8,24 @@ import {Component, HostBinding, OnInit} from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-    this.title = 'Angular';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+this.article = new Article(
+  'Angular',
+  'http://angular.io',
+  10
+);
 
   }
 
-  voteUp():boolean {
-    this.votes += 1;
+  voteUp(): boolean {
+    this.article.voteUp();
     return false;
   }
 
-  voteDown() : boolean{
-    this.votes -= 1;
+  voteDown(): boolean {
+    this.article.voteDown();
     return false;
   }
 
